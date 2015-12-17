9b3a54c11901215ac1e09297fe283c74a04bc016
Oriane <oriane.mv@gmail.com>
2015-12-17 16:25:19 +0100

    profile page mustache

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-	$('a').click(function(event){
+	$('a').mouseenter(function(event){
 		event.preventDefault();
 
 
@@ -13,17 +13,41 @@ $(document).ready(function(){
 
 	});
 
+// 	$.getJSON('http://vps227573.ovh.net/u-8.json', function(data){
+// 	var tutu= $('[elt]');
+// 	for(var m=0; m<tutu.length; m ++){
+// 		var attribut= $(tutu[m]).attr("elt");
+// 		var valeur= data [attribut];
+// 		$(tutu[m]).html(valeur);
+
+
+// 	// console.log(data);
+// }
+// });
+
+	
+		//     // var Full_Name = '{{first_name}} {{last_name}}';
+		//     var infoFull_Name = Mustache.to_html('{{first_name}} {{last_name}}', data);
+		//     $('h1').html(infoFull_Name);
+		    
+
+		// 	// var occupation = '{{occupation}}';
+		//     var infooccupation = Mustache.to_html('{{occupation}}', data);
+		//     $('h3').html(infooccupation);
+		// });
+
+
+	$.getJSON('http://vps227573.ovh.net/u-8.json', function(data) {
+
+		var template =$('#template').html();
+		var rendered = Mustache.to_html(template, data);
+		$('body').html(rendered);
+
+
 });
 
-$.getJSON("user-8.json", function(data){
-	var tutu= $("[elt]");
-	for(var m=0; m<tutu.length; m ++){
-		var attribut= $(tutu[m]).attr("elt");
-		var valeur= data [attribut];
-		$(tutu[m]).html(valeur);
 
-
-	console.log(data);
-}
 });
+
+
 
